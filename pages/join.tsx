18d05610045daddc94f49e4b636fcaf6807bfd2c
Aradhya-Shaswat <reddye1120@gmail.com>
2022-12-21 18:53:19 +0530

    v.4

--- pages/join.tsx
+++ pages/join.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
 import Link from "next/link"
+//hi
 
 const inter = Inter({ subsets: ['latin'] })
 
