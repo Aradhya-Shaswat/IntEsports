31c870cf05b6daefde727a521f8a3fa12bf44afe
Aradhya-Shaswat <reddye1120@gmail.com>
2022-12-21 18:43:15 +0530

    v.1

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,6 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
 import Link from "next/link"
-import React, { useEffect, useState } from "react";
-
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
